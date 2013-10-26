f6d58359a765c4686de830603626b3f81b8bafce
Alex Sanders <aguerrra2008@yahoo.com>
2013-10-26 10:27:06 -0700

    Homework 3 Done

--- homework3/dna_analysis.py
+++ homework3/dna_analysis.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Name:Alex Sanders
 # Evergreen Login: Sanale04
 # Computer Science Foundations
@@ -14,12 +15,14 @@
 ###
 
 # The sys module supports reading files, command-line arguments, etc.
+
 import sys
 
 
 ###########################################################################
 ### Read the nucleotides into a variable named seq
 ###
+
 
 # You need to specify a file name
 if len(sys.argv) < 2:
@@ -30,10 +33,13 @@ filename = sys.argv[1]
 # A file object from which data can be read.
 inputfile = open(filename)
 
+
 # All the nucleotides in the input file that have been read so far.
 seq = ""
 # The current line number (= the number of lines read so far).
 linenum = 0
+
+
 
 
 for line in inputfile:
@@ -45,14 +51,24 @@ for line in inputfile:
         seq = seq + line
 
 
+
+
 ###########################################################################
 ### Compute statistics
 ###
+
 
 # Total nucleotides seen so far.
 total_count = 0
 # Number of G and C nucleotides seen so far.
 gc_count = 0
+at_count = 0
+g_count  = 0
+c_count  = 0
+a_count  = 0
+t_count  = 0
+
+
 
 
 # for each base pair in the string,
@@ -60,14 +76,45 @@ for bp in seq:
     # increment the total number of bps we've seen
     total_count = total_count + 1
 
+
     # next, if the bp is a G or a C,
     if bp == 'C' or bp == 'G':
         # increment the count of gc
         gc_count = gc_count + 1
+        if bp == 'C':
+            c_count +=1
+        elif bp == 'G':
+            g_count +=1
+
+
+for at in seq:
+    if at == 'A' or at == 'T':
+        # increment the count of at
+        at_count = at_count + 1
+        if at == 'A':
+            a_count +=1
+        elif at == 'T':
+            t_count +=1
+
+
+sum_count = g_count + c_count + a_count + t_count
 
 
 # divide the gc_count by the total_count
-gc_content = float(gc_count) / total_count
+gc_content = float(gc_count) / sum_count
+at_content = float(at_count) / sum_count
+
 
 # Print the answer
+print 'G Count:', g_count
+print 'C Count:', c_count
+print 'A count:', a_count
+print 'T count:', t_count
+print 'Sum count:', sum_count
+print 'GC Count:', gc_count #for testing
+print 'AT count:', at_count
+print 'Total Count:', total_count
+
+
 print 'GC-content:', gc_content
+print 'AT-content:', at_content
